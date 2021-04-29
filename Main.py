0dffc612dc73113a6b9dd2c62320fbcae2d18586
Ahmed-OT <75503438+Ahmed-OT@users.noreply.github.com>
2021-04-28 21:53:41 -0400

    KG 1
    By adding the sys function we make the code flexible.

---
+++ Main.py
@@ -0,0 +1,26 @@
+import sys
+
+c=sys.argv[1:]
+if len(c)==0:
+    print("No Argument Given")
+    sys.exit()
+    
+num={"0":"Zero",
+    "1":"One",
+    "2":"Two",
+    "3":"Three",
+    "4":"Four",
+    "5":"Five",
+    "6":"Six",
+    "7":"Seven",
+    "8":"Eight",
+    "9":"Nine"}
+line=""
+
+for x in c:
+    for y in x:
+        line=line+num[y]
+    line=line+","
+line=line[0:-1]
+
+print(line)
